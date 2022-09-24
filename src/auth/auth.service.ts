e1ce584886dbd03f523351bcaa0dd305c4ca4eee
Mateusz <nowe7246@gmail.com>
2022-09-24 13:04:24 +0200

    Created sign token function and get token after login.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,16 @@ import {AuthLoginDto} from "./dto/auth-login.dto";
 import * as bcrypt from 'bcrypt';
 import {PrismaService} from "../prisma/prisma.service";
 import {PrismaClientKnownRequestError} from '@prisma/client/runtime';
-
+import {JwtService} from "@nestjs/jwt";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService) {
+    }
 
     async signup(dto) {
         const salt = await bcrypt.genSalt(10);
@@ -42,7 +47,23 @@ export class AuthService {
         const isCorrectPassword = await bcrypt.compare(dto.hash, user.hash);
         if (!isCorrectPassword) throw new ForbiddenException('Nie prawidłowe hasło')
 
-        delete user.hash
-        return user
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: string, email: string):Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '2d',
+            secret,
+        });
+
+        return {
+            access_token: token,
+        };
     }
 }
