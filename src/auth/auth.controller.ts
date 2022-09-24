2aff99411bdf47cb2cba42faa2b50da1f27bb6f0
Mateusz <nowe7246@gmail.com>
2022-09-24 12:15:15 +0200

    Created sign in and login routes.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import { AuthLoginDto} from "./dto/auth-login.dto";
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private authService: AuthService) {
+    }
+
+    @Post('signup')
+    signup(@Body() dto) {
+        return this.authService.signup(dto)
+    }
+
+
+    @Post('login')
+    login(@Body() dto:AuthLoginDto) {
+        return this.authService.login(dto);
+    }
 }
