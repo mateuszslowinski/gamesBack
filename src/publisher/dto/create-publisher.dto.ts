338d7a5b49577fd5efe8a90f7e64a0e7d2f1d2c5
Mateusz <nowe7246@gmail.com>
2022-09-25 12:05:21 +0200

    Added created pulisher dto.

--- src/publisher/dto/create-publisher.dto.ts
+++ src/publisher/dto/create-publisher.dto.ts
@@ -1,1 +1,8 @@
-export class CreatePublisherDto {}
+import {IsNotEmpty, IsString, Length} from "class-validator";
+
+export class CreatePublisherDto {
+    @IsString()
+    @Length(1, 50)
+    @IsNotEmpty()
+    name:string;
+}
