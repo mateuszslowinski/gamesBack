067201375fde5a097b09e4a8e004ccfcaabde763
Mateusz <nowe7246@gmail.com>
2022-09-26 16:47:48 +0200

    Added update game dto.

--- src/game/dto/update-game.dto.ts
+++ src/game/dto/update-game.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGameDto } from './create-game.dto';
+import {IsOptional, IsString, Length} from "class-validator";
 
-export class UpdateGameDto extends PartialType(CreateGameDto) {}
+export class UpdateGameDto extends PartialType(CreateGameDto) {
+    @IsString()
+    @Length(1, 100)
+    @IsOptional()
+    name: string;
+
+    @IsString()
+    @Length(1, 36)
+    @IsOptional()
+    developerId: string
+
+    @IsString()
+    @IsOptional()
+    releaseDate: string
+
+    @IsString()
+    @Length(0, 1000)
+    @IsOptional()
+    description: string
+
+    @IsString()
+    @Length(0, 80)
+    @IsOptional()
+    image: string
+}
