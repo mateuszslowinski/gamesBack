452c4f16e3c0936d0a5fae2c942277c83e080795
Mateusz <nowe7246@gmail.com>
2022-09-26 10:39:36 +0200

    Added create-studio dto.

--- src/studio/dto/create-studio.dto.ts
+++ src/studio/dto/create-studio.dto.ts
@@ -1,1 +1,27 @@
-export class CreateStudioDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Length} from "class-validator";
+
+export class CreateStudioDto {
+
+    @IsString()
+    @Length(1, 100)
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @Length(1, 50)
+    @IsNotEmpty()
+    country: string
+
+    @IsString()
+    @Length(1, 36)
+    ownerId: string
+
+    @IsString()
+    @Length(0, 4)
+    @IsOptional()
+    founded?: string
+
+    @IsNumber()
+    @IsOptional()
+    employees?: number
+}
