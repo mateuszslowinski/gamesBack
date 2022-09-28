e903039433638be24e8d19ec261bd426aa13d703
Mateusz <nowe7246@gmail.com>
2022-09-28 10:05:11 +0200

    Added cors and prefix settings to application.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    app.setGlobalPrefix('api');
+
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
+
     await app.listen(3001);
 }
 
