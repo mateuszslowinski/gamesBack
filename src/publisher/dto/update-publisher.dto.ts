413c91225dcd4cfad293b2039661fec137890ff6
Mateusz <nowe7246@gmail.com>
2022-10-27 15:30:38 +0200

    Changed updated publisher dto by remove name.

--- src/publisher/dto/update-publisher.dto.ts
+++ src/publisher/dto/update-publisher.dto.ts
@@ -2,11 +2,6 @@ import { IsOptional, IsString, Length} from "class-validator";
 
 export class UpdatePublisherDto {
     @IsString()
-    @Length(1, 50)
-    @IsOptional()
-    name:string;
-
-    @IsString()
     @Length(1, 1500)
     @IsOptional()
     description:string;
