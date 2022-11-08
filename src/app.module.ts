c6b126f075f4c04ef30450a9c8a739e7f783c4dd
Mateusz <nowe7246@gmail.com>
2022-11-08 08:48:01 +0100

    Changed port for env.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,25 @@
-import {Module} from '@nestjs/common';
-import {PrismaModule} from "./prisma/prisma.module";
-import {ConfigModule} from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import {UserModule} from "./user/user.module";
+import { UserModule } from './user/user.module';
 import { PublisherModule } from './publisher/publisher.module';
 import { StudioModule } from './studio/studio.module';
 import { PlatformModule } from './platform/platform.module';
 import { GameModule } from './game/game.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-        }),
-        PrismaModule,
-        AuthModule,
-        UserModule,
-        PublisherModule,
-        StudioModule,
-        PlatformModule,
-        GameModule
-    ],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    PublisherModule,
+    StudioModule,
+    PlatformModule,
+    GameModule,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,30 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {ValidationPipe} from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api');
-    app.enableCors({
-        origin: 'http://localhost:3000',
-        credentials: true,
-    });
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            disableErrorMessages: true,
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
 
-            whitelist: true,
-            forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
 
-            transform: true,
-            transformOptions: {
-                enableImplicitConversion: true,
-            },
-        }),
-    );
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
+  await app.listen(process.env.PORT || 8080);
 
-    await app.listen(3001);
+  bootstrap();
 }
-
-bootstrap();
